22a02f0f549f1a5eb036d137f380dcde136feefc
kwhitejr <kwhitejr.edu@gmail.com>
2016-02-13 10:37:31 -1000

    clockwise and counterclockwise okay

--- MatrixRotator.js
+++ MatrixRotator.js
@@ -1,7 +1,7 @@
 /*  MatrixRotator(matrix)
  *
  *  @param matrix                        a multidimensional array containing the matrix
- *  
+ *
  *  @public property matrix              the matrix
  *
  *  @public method rotate(direction)     direction is either
@@ -13,15 +13,48 @@ var Direction = require("./Direction").Direction;
 
 function MatrixRotator(matrix){
   this.matrix = matrix;
-  
+
 };
 
 //                                         |-- Must be Direction.CW
 //                                         v        or Direction.CCW
 MatrixRotator.prototype.rotate = function(direction) {
-  // do work here
+  var newMatrix = [];
+  for(var i = 0; i<this.matrix.length; i++) {
+      newMatrix.push([]);
+    }
 
-  // must be a valid Direction, see Direction.js
+  if (!Array.isArray(this.matrix)) {
+    console.log('this is not an array');
+    throw new Error;
+  }
 
+  if (this.matrix.length !== this.matrix[0].length) {
+    console.log('this is not a valid matrix');
+    throw new Error;
+  }
 
+  if (direction === Direction.CW) {
+
+    for (var m = 0; m < this.matrix.length; m ++) {
+      for(var k = 0; k<this.matrix.length; k++) {
+       newMatrix[k].unshift(this.matrix[m][k]);
+      }
+    }
+    console.log(newMatrix);
+    this.matrix = newMatrix;
+    return this.matrix;
+  }
+
+  if (direction === Direction.CCW) {
+
+    for (var r = 0; r < this.matrix.length; r++) {
+      for(var t = this.matrix.length - 1; t>=0; t--) {
+        newMatrix[t].push(this.matrix[r][(this.matrix.length - 1) - t]);
+      }
+    }
+    console.log(newMatrix);
+    this.matrix = newMatrix;
+    return this.matrix;
+  }
 };
